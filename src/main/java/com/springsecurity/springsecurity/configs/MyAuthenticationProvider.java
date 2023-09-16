20db029ae017ae7b56699651f12fdbddb01281f3
Karthik <msdhamodharan5@gmail.com>
2023-09-16 16:34:02 +0530

    Custom Auth Provider

---
+++ src/main/java/com/springsecurity/springsecurity/configs/MyAuthenticationProvider.java
@@ -0,0 +1,31 @@
+package com.springsecurity.springsecurity.configs;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class MyAuthenticationProvider  implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String userName = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        if("root".equals(userName) && "root".equals(password)){
+            return new UsernamePasswordAuthenticationToken(userName,password, Arrays.asList());
+        }else{
+            throw new BadCredentialsException("Invalid Username and Password");
+        }
+
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
--- src/main/java/com/springsecurity/springsecurity/configs/SecurityConfig.java
+++ src/main/java/com/springsecurity/springsecurity/configs/SecurityConfig.java
@@ -3,11 +3,29 @@ package com.springsecurity.springsecurity.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+//    @Bean
+//    UserDetailsService userDetailsService(){
+//        InMemoryUserDetailsManager userDetailService = new InMemoryUserDetailsManager();
+//        UserDetails user = User.withUsername("root")
+//                .password(passwordEncoder().encode("root")).authorities("read").build();
+//        userDetailService.createUser(user);
+//        return userDetailService;
+//    }
+
+//    @Bean
+//    BCryptPasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
     @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.httpBasic();
